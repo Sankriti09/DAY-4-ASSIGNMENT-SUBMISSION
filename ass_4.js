70a0ee67c3b4f856cf1521b57342bbbd1a4d315c
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2021-06-16 22:27:30 +0530

    Add files via upload

---
+++ ass_4.js
@@ -0,0 +1,8 @@
+function addNumbers(){
+    var a,b,c;
+    a=Number(document.getElementById("first").value);
+    b=Number(document.getElementById("second").value);
+    c= a + b;
+    document.getElementById("answer").value= c;
+}
+    
